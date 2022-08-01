d4f09b345e9652799f09e35c27f3dd3935758b22
OQueLucas <lucasestevam365@gmail.com>
2022-08-01 16:47:10 -0300

    adding typescript, using express to create routes and configuring to rebuild project automatically using ts-node-dev

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Executei!")
+import express, { Request, Response, NextFunction } from 'express'
+
+const app = express();
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ foo: 'sucesso total!'})
+})
+
+app.listen(3000, () => {
+  console.log('Aplicação executando na porta 3000!')
+})
