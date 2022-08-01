d6b0837bc4cebef2b6f40c0f89771a10d0bda1ff
OQueLucas <lucasestevam365@gmail.com>
2022-08-01 18:32:34 -0300

    adding lasts cruds

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,12 +1,6 @@
 import { Router, Request, Response, NextFunction } from "express";
 import { StatusCodes } from 'http-status-codes';
 
-// get /users
-// get /users:uuid
-// post /users
-// put /users/:uuid
-// delete /users/:uuid
-
 const usersRoute =  Router();
 
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
@@ -22,6 +16,19 @@ usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, n
 usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
   const newUser = req.body
   res.status(StatusCodes.CREATED).send(newUser)
+})
+
+usersRoute.put('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+  const uuid = req.params.uuid;
+  const modifiedUser = req.body;
+
+  modifiedUser.uuid = uuid;
+
+  res.status(StatusCodes.OK).send({ modifiedUser });
+})
+
+usersRoute.delete('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) =>  {
+  res.sendStatus(StatusCodes.OK);
 })
 
 export default usersRoute;
