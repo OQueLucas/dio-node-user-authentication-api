72b2663fee351bb10bea4a7c7c10a8323db047fe
OQueLucas <lucasestevam365@gmail.com>
2022-08-01 18:37:47 -0300

    create route to status

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,18 @@
-import express, { Request, Response, NextFunction } from 'express'
-import usersRoute from './routes/users.route';
+import express from "express";
+import statusRoute from "./routes/status.route";
+import usersRoute from "./routes/users.route";
 
 const app = express();
 
 // Configurações da aplicação
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 // Configurações de Rotas
-app.use(usersRoute)
-
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({ foo: 'sucesso total!'})
-})
+app.use(usersRoute);
+app.use(statusRoute);
 
 // Inicialização do servidor
 app.listen(3000, () => {
-  console.log('Aplicação executando na porta 3000!')
-})
+  console.log("Aplicação executando na porta 3000!");
+});
---
+++ src/routes/status.route.ts
@@ -0,0 +1,10 @@
+import { StatusCodes } from 'http-status-codes';
+import { Router, Request, Response, NextFunction } from "express";
+
+const statusRoute = Router();
+
+statusRoute.get('/status', (req: Request, res: Response, next: NextFunction) => {
+  res.sendStatus(StatusCodes.OK)
+})
+
+export default statusRoute;
